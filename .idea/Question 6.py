49e7f19d95bd899ac3922acc362f6b40fe4c8abf
ThebergeV <100051037+ThebergeV@users.noreply.github.com>
2023-03-30 23:20:56 -0400

    Comments and question 6

--- .idea/Question 6.py
+++ .idea/Question 6.py
@@ -2,5 +2,23 @@
 import numpy as np
 import math
 
-#length = Question 4()
+minA = -1
+maxA = -1
+a = 0
 
+#for a parameter a, runs the code of question 3 and 4 to find the length of the curve
+while error >= 0.00001:
+    l = length(a)
+    errorMin = 0
+    errorMax = 0
+    #Replaces the value of a if it give the minimum length of the curve or the maximum
+    if (minA < 0 or l < minA):
+        errorMin = abs(minA - l)
+        minA = l
+        aMin = a
+    if (maxA < 0 or l > maxA):
+        errorMax = abs(maxA - l)
+        maxA = l
+        aMax = a
+    error = max(errorMin, errorMax)
+    a = a + error
