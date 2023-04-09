b105ef2ae6f4d4f370ba25a462206c1f1e99fc67
ThebergeV <100051037+ThebergeV@users.noreply.github.com>
2023-04-09 02:49:20 -0400

    changes to 6

--- .idea/Question_6.py
+++ .idea/Question_6.py
@@ -1,6 +1,9 @@
 import numpy as np
 import math
 from Question_4 import *
+from Question_7 import *
+
+domain = findDomain(-200, 200)
 #Example for gitHub
 minCurve = -1
 maxCurve = -1
@@ -9,8 +12,8 @@ error = 9999
 h = 0
 
 #for a parameter a, runs the code of question 3 and 4 to find the length of the curve
-while error >= 0.00001:
-    l = findLength(a, h)
+for i in range(min(domain), max(domain)):
+    l = findLength(, h)
     errorMin = 0
     errorMax = 0
     #Replaces the value of a if it give the minimum length of the curve or the maximum
