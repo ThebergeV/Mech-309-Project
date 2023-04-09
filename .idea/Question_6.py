793cc572e08d921c750a9d507f451c901603dc70
ThebergeV <100051037+ThebergeV@users.noreply.github.com>
2023-04-09 04:54:20 -0400

    Final modifs?

--- .idea/Question_6.py
+++ .idea/Question_6.py
@@ -32,7 +32,7 @@ print(completeDomain)
 #for a parameter b, runs the code of question 3 and 4 to find the length of the curve
 numb =0
 minCurve = 9999999999999999999999999
-maxCurve = 9999999999999999999999999
+maxCurve = 0
 for i in completeDomain:
     if numb != 0:
         l = findLength(a, h, i)
